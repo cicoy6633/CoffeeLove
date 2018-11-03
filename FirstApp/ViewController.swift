70d774d9e9262b25fedd540db678876ac0b985ad
Yi Ting Lu <yitinglu@yis-macbook-air.local>
2018-11-03 18:22:24 +0800

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Yi Ting Lu on 2018/11/3.
+//  Copyright © 2018 Yi Ting Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
